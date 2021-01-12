a7ff47ce91fba92cf17c1c9088fed241164d21be
shrddr <qwerty.zigzag@gmail.com>
2021-01-12 21:38:54 +0300

    browser 4

--- Text-Based Browser/browser.py
+++ Text-Based Browser/browser.py
@@ -1,68 +1,35 @@
 import os.path
 import argparse
-
-nytimes_com = '''
-This New Liquid Is Magnetic, and Mesmerizing
-
-Scientists have created “soft” magnets that can flow 
-and change shape, and that could be a boon to medicine 
-and robotics. (Source: New York Times)
-
-
-Most Wikipedia Profiles Are of Men. This Scientist Is Changing That.
-
-Jessica Wade has added nearly 700 Wikipedia biographies for
- important female and minority scientists in less than two 
- years.
-
-'''
-
-bloomberg_com = '''
-The Space Race: From Apollo 11 to Elon Musk
-
-It's 50 years since the world was gripped by historic images
- of Apollo 11, and Neil Armstrong -- the first man to walk 
- on the moon. It was the height of the Cold War, and the charts
- were filled with David Bowie's Space Oddity, and Creedence's 
- Bad Moon Rising. The world is a very different place than 
- it was 5 decades ago. But how has the space race changed since
- the summer of '69? (Source: Bloomberg)
-
-
-Twitter CEO Jack Dorsey Gives Talk at Apple Headquarters
-
-Twitter and Square Chief Executive Officer Jack Dorsey 
- addressed Apple Inc. employees at the iPhone maker’s headquarters
- Tuesday, a signal of the strong ties between the Silicon Valley giants.
-'''
+import requests
 
 argparser = argparse.ArgumentParser()
 argparser.add_argument("dir")
 args = argparser.parse_args()
 
-content = {'nytimes.com': nytimes_com, 'bloomberg.com': bloomberg_com}
 
+def display(url, dot_position):
 
-def display(url):
-    if url in content:
-        filename = url[:dot_position]
-        filepath = os.path.join(args.dir, filename)
+    filename = url[:dot_position]
+    filepath = os.path.join(args.dir, filename)
 
-        if os.path.exists(filepath):
-            with open(filepath) as f:
-                page = f.read()
-        else:
-            page = content[url]
-            if not os.path.exists(args.dir):
-                os.mkdir(args.dir)
-            if not os.path.isdir(args.dir):
-                print("Error: dir exists but is not a dir")
-            with open(filepath, 'w') as f:
-                f.write(page)
-
-        print(page)
+    if os.path.exists(filepath):
+        with open(filepath) as f:
+            page = f.read()
     else:
-        print("Error: unknown url")
+        if url[:8] != 'https://':
+            url = 'https://' + url
+        r = requests.get(url)
+        page = r.text
+
+        if not os.path.exists(args.dir):
+            os.mkdir(args.dir)
+        if not os.path.isdir(args.dir):
+            print("Error: dir exists but is not a dir")
+        with open(filepath, 'w', encoding='utf8') as f:
+            f.write(page)
+
+    print(page)
+
 
 
 history = []
@@ -77,7 +44,7 @@ while True:
 
     dot_position = cmd.find('.')
     if dot_position >= 0:
-        display(cmd)
+        display(cmd, dot_position)
         history.append(cmd)
     else:
         print('Error: no dot')
