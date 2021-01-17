261213c011733f17e0e21db868c93a8ca5210b94
shrddr <qwerty.zigzag@gmail.com>
2021-01-17 19:54:18 +0300

    calc 3

--- Smart Calculator/calculator.py
+++ Smart Calculator/calculator.py
@@ -3,8 +3,10 @@ while True:
     if line == '/exit':
         print('Bye!')
         break
+    if line == '/help':
+        print('The program calculates the sum of numbers')
+        continue
     args = [int(s) for s in line.split()]
-    if len(args) == 1:
-        print(args[0])
-    if len(args) == 2:
-        print(args[0] + args[1])
+    if len(args) > 0:
+        res = sum(args)
+        print(res)
