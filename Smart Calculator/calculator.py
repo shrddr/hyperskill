0737a6513c0b09d7d6b3e1092da6ebbc6b7cadbb
shrddr <qwerty.zigzag@gmail.com>
2021-01-17 22:23:34 +0300

    calc 4

--- Smart Calculator/calculator.py
+++ Smart Calculator/calculator.py
@@ -1,12 +1,70 @@
+class NoOpError(Exception):
+    pass
+
+def try_int(s):
+    try:
+        return int(s)
+    except ValueError:
+        return None
+
+class Expr:
+    def __init__(self):
+        self.arg = None
+        self.op = None
+
+    def complete(self):
+        return self.arg is not None and self.op is not None
+
+    def compute(self, left):
+        if self.op == '+':
+            return left + self.arg
+        if self.op == '-':
+            return left - self.arg
+
+    def parse_op(self, s):
+        for c in s:
+            if self.op is None:
+                self.op = c
+            else:
+                if c == '-':
+                    self.op = '-' if self.op == '+' else '+'
+
+    def parse_arg(self, i):
+        if self.arg is None:
+            self.arg = i
+        else:
+            raise NoOpError
+
+    def parse(self, s):
+        i = try_int(s)
+        if i is None:
+            self.parse_op(s)
+        else:
+            self.parse_arg(i)
+
+
 while True:
     line = input()
+
     if line == '/exit':
         print('Bye!')
         break
     if line == '/help':
-        print('The program calculates the sum of numbers')
+        print('The program calculates the +- of numbers')
         continue
-    args = [int(s) for s in line.split()]
-    if len(args) > 0:
-        res = sum(args)
+    args = line.split()
+    res = None
+    e = Expr()
+    for s in args:
+        if res is None:
+            res = int(s)
+            continue
+        e.parse(s)
+        if e.complete():
+            res = e.compute(res)
+            e = Expr()
+    if res is not None:
         print(res)
+
+
+
