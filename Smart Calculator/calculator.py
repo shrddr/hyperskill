a8798de98f63793e10db7930055dafdbb858ff54
shrddr <qwerty.zigzag@gmail.com>
2021-01-17 22:37:05 +0300

    calc 5

--- Smart Calculator/calculator.py
+++ Smart Calculator/calculator.py
@@ -1,11 +1,13 @@
-class NoOpError(Exception):
+class InvalidExpression(Exception):
     pass
+
 
 def try_int(s):
     try:
         return int(s)
     except ValueError:
         return None
+
 
 class Expr:
     def __init__(self):
@@ -21,50 +23,60 @@ class Expr:
         if self.op == '-':
             return left - self.arg
 
-    def parse_op(self, s):
-        for c in s:
-            if self.op is None:
-                self.op = c
-            else:
-                if c == '-':
-                    self.op = '-' if self.op == '+' else '+'
+    def parse_op(self, c):
+        if self.op is None:
+            self.op = c
+        else:
+            if c == '-':
+                self.op = '-' if self.op == '+' else '+'
 
     def parse_arg(self, i):
         if self.arg is None:
             self.arg = i
         else:
-            raise NoOpError
+            raise InvalidExpression
 
     def parse(self, s):
         i = try_int(s)
         if i is None:
-            self.parse_op(s)
+            for c in s:
+                if c in ['-', '+']:
+                    self.parse_op(c)
+                else:
+                    raise InvalidExpression
+
         else:
             self.parse_arg(i)
 
 
 while True:
     line = input()
+    if len(line) and line[0] == '/':
+        if line == '/exit':
+            print('Bye!')
+            break
+        elif line == '/help':
+            print('The program calculates the +- of numbers')
+            continue
+        else:
+            print('Unknown command')
+            continue
 
-    if line == '/exit':
-        print('Bye!')
-        break
-    if line == '/help':
-        print('The program calculates the +- of numbers')
-        continue
     args = line.split()
     res = None
     e = Expr()
-    for s in args:
-        if res is None:
-            res = int(s)
-            continue
-        e.parse(s)
-        if e.complete():
-            res = e.compute(res)
-            e = Expr()
-    if res is not None:
-        print(res)
+    try:
+        for s in args:
+            if res is None:
+                res = int(s)
+                continue
+            e.parse(s)
+            if e.complete():
+                res = e.compute(res)
+                e = Expr()
+        if res is not None:
+            print(res)
 
-
-
+    except (ValueError, InvalidExpression):
+        print('Invalid expression')
+        continue
