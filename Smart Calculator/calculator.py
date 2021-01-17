8c0e818396324f5acdc1fb8852081184ac33cb98
shrddr <qwerty.zigzag@gmail.com>
2021-01-17 19:51:31 +0300

    calc 2

--- Smart Calculator/calculator.py
+++ Smart Calculator/calculator.py
@@ -1,4 +1,10 @@
-line = input()
-args = [int(s) for s in line.split()]
-if len(args) == 2:
-    print(args[0] + args[1])
+while True:
+    line = input()
+    if line == '/exit':
+        print('Bye!')
+        break
+    args = [int(s) for s in line.split()]
+    if len(args) == 1:
+        print(args[0])
+    if len(args) == 2:
+        print(args[0] + args[1])
